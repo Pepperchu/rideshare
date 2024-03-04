fbd24402885a34ca39542603a8fcf0db602de51d
Pepperchu <123325290+Pepperchu@users.noreply.github.com>
2024-03-03 23:00:47 -0500

    Update SpawnOnMap.cs

--- Assets/Assets/Mapbox/Examples/6_ZoomableMap/Scripts/SpawnOnMap.cs
+++ Assets/Assets/Mapbox/Examples/6_ZoomableMap/Scripts/SpawnOnMap.cs
@@ -1,4 +1,4 @@
-﻿namespace Mapbox.Examples
+namespace Mapbox.Examples
 {
 	using UnityEngine;
 	using Mapbox.Utils;
@@ -38,14 +38,17 @@
 
 		List<GameObject> _spawnedObjects;
 
-		void Start()
+        public string timeOfDay; 
+
+	void Start()
     {
         _spawnedObjects = new List<GameObject>();
     }
 
-		public void GenerateAndSpawnMarkers()
+	public void GenerateAndSpawnMarkers()
     {
         //ClearSpawnedMarkers();
+        numberOfLocations = GetNumberOfMarkers(timeOfDay);
         GenerateRandomLocations();
         SpawnMarkers(_markerPrefab);
     }
@@ -63,6 +66,61 @@ void GenerateRandomLocations()
     }
 }
 
+public void MorningButtonClick()
+{
+    timeOfDay = "Morning";
+    Debug.Log("Time of day set to: " + timeOfDay);
+}
+
+public void AfternoonButtonClick()
+{
+    timeOfDay = "Afternoon";
+    Debug.Log("Time of day set to: " + timeOfDay);
+}
+
+public void NightButtonClick()
+{
+    timeOfDay = "Night";
+    Debug.Log("Time of day set to: " + timeOfDay);
+}
+
+private float TriangularDistribution(float min, float max, float mode)
+{
+    ///Triangular Distribution: 
+    ///for 0 < u < max -> a + sqrt[u(b-a)(c-a)]
+    ///for max <= u < 1 -> b - sqrt[(1-u)(b-a)(b-c)]
+
+    float u = Random.value; 
+
+ if (u <= (mode - min) / (max - min))
+    {
+        return min + Mathf.Sqrt(u * (max - min) * (mode - min));
+    }
+    else ///random number falls within max <= u < 1
+    {
+        return max - Mathf.Sqrt((1 - u) * (max - min) * (max - mode));
+    }
+   
+}
+
+public int GetNumberOfMarkers(string timeOfDay)
+{
+    
+    switch (timeOfDay)
+    {
+        case "Morning":
+            return Mathf.RoundToInt(TriangularDistribution(0.1f, 0.5f, 0.2f) * numberOfLocations);
+        case "Afternoon":
+            return Mathf.RoundToInt(TriangularDistribution(0.3f, 0.7f, 0.5f) * numberOfLocations);
+       case "Night":
+            return Mathf.RoundToInt(TriangularDistribution(0.6f, 0.9f, 0.8f) * numberOfLocations);
+        default:
+            Debug.LogError("Please select a time of day.");
+            return 0; 
+
+    }
+}
+
 public void SetNumberOfLocationsAndSpawn(int count)
 {
     numberOfLocations = count;
@@ -70,7 +128,7 @@ public void SetNumberOfLocationsAndSpawn(int count)
 }
 
 
-		public void SpawnMarkers(GameObject prefabToSpawn)
+	public void SpawnMarkers(GameObject prefabToSpawn)
     {
         for (int i = 0; i < numberOfLocations; i++)
         {
